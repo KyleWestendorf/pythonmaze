54d70f21c5abdeb2515c30f34782b5e642b755ad
KyleWestendorf <kylewestendorf@Gmail.com>
2019-01-10 19:29:14 -0500

    Added for loop to evaluateBoard to determine if evaluateBoard contains an E

--- maze_follower.py
+++ maze_follower.py
@@ -2,4 +2,9 @@ def test():
     return True
 
 def evaluateBoard(maze):
+    for i, row in enumerate(maze):
+        for j, position in enumerate(row):
+            if position == "E":
+                raise ValueError
+
     raise ValueError
--- test/test_maze_follower.py
+++ test/test_maze_follower.py
@@ -5,8 +5,16 @@ def test_test():
     result = test()
     assert(result == True)
 
-def test_given_user_has_received_a_board_that_lacks_a_start_and_end_when_evaluating_then_an_error_is_thrown_missing_s_and_missing_e():
+def test_given_user_has_received_a_board_that_lacks_a_start_and_end_when_evaluating_the_board_then_an_error_is_thrown_missing_s_and_missing_e():
     maze = [""]
+    try:
+        evaluateBoard(maze)
+        assert(False)
+    except ValueError:
+        return
+
+def test_given_user_has_received_a_board_that_lacks_a_start_when_evaluating_the_board_then_an_error_is_thrown_missing_s():
+    maze = ["E"]
     try:
         evaluateBoard(maze)
         assert(False)
